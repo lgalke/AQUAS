5165d816a9da8a95b4b17da6f2555098ca5f499c
Lukas Galke <git@lpag.de>
2023-04-21 15:09:45 +0200

    remove tf, add eval code, add wandb integration

--- analysis/BERT-training-sliding-window.py
+++ analysis/BERT-training-sliding-window.py
@@ -8,8 +8,15 @@ __license__ = "ISC license"
 __email__ = "seidlmayer@zbmed.de"
 __version__ = "1 "
 
+# BERT_MODEL_IDENTIFIER = "bert-base-uncased"
+BERT_MODEL_IDENTIFIER = "dmis-lab/biobert-v1.1"
+
 import pandas as pd
-from transformers import BertTokenizer, BertForSequenceClassification, BertForPreTraining
+from transformers import (
+    BertTokenizer,
+    BertForSequenceClassification,
+    BertForPreTraining,
+)
 import numpy as np
 import argparse
 import os
@@ -20,52 +27,61 @@ from typing import Optional, Union, Tuple
 from transformers.modeling_outputs import SequenceClassifierOutput
 
 
+try:
+    import wandb
+except ImportError:
+    print("Wandb not installed. Not using wandb. To use: pip install wandb")
+
+
 def load_dataset(input_file_csv):
     # Load dataset
-    df = pd.read_csv(input_file_csv, sep=',')
+    df = pd.read_csv(input_file_csv, sep=",")
     df = df.sample(frac=1)
     df = df.astype(str)
-    texts = df['text'].to_list()
-    labels = df['category-id'].to_list()
-    print('data input lists created')
+    texts = df["text"].to_list()
+    labels = df["category-id"].to_list()
+    print("data input lists created")
     return texts, labels
 
 
 def tokenize(texts):
     # Load the tokenizer
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_IDENTIFIER)
 
     # set max_length
     max_length = 100000
 
     # Tokenize the text data
-    tokens = tokenizer(texts, max_length=max_length, padding='max_length', truncation=True)
-    print('text is tokenized')
+    tokens = tokenizer(
+        texts, max_length=max_length, padding="max_length", truncation=True
+    )
+    print("text is tokenized")
     return tokens
 
 
 def convert_labels(labels):
     # Convert labels to numerical values
-    label_map = {'1': 0, '2': 1, '3': 2}
+    label_map = {"1": 0, "2": 1, "3": 2}
     labels = [label_map[label] for label in labels]
     labels_conv = torch.tensor(labels)
 
-    print('labels converted')
+    print("labels converted")
     return labels_conv
 
 
 def calc_split_ratio(labels_conv):
     # 80% training 20% validation
     split_ratio = int(len(labels_conv) * 0.2)
-    print('split_ratio defined')
+    print("split_ratio defined")
     return split_ratio
+
 
 def split_train_val_data(tokens, split_ratio, labels_conv):
     # Split the data into training and validation sets
-    train_inputs, val_inputs = np.split(tokens['input_ids'], [split_ratio])
-    train_masks, val_masks = np.split(tokens['attention_mask'], [split_ratio])
+    train_inputs, val_inputs = np.split(tokens["input_ids"], [split_ratio])
+    train_masks, val_masks = np.split(tokens["attention_mask"], [split_ratio])
     train_labels, val_labels = np.split(labels_conv, [split_ratio])
-    print('train/val -inputs, -masks, -labels created')
+    print("train/val -inputs, -masks, -labels created")
     return train_inputs, val_inputs, train_masks, val_masks, train_labels, val_labels
 
 
@@ -73,34 +89,39 @@ def split_train_val_data(tokens, split_ratio, labels_conv):
 def sliding_window(item):
     window_size = 512
     stride = 256
-    windows = [item[i:i+window_size] for i in range(0, len(item)-window_size+1, stride)]
+    windows = [
+        item[i : i + window_size] for i in range(0, len(item) - window_size + 1, stride)
+    ]
     return windows
+
 
 class AQUASSlidingBERT(BertForSequenceClassification):
     def forward(
-            self,
-            input_ids: Optional[torch.Tensor] = None,
-            attention_mask: Optional[torch.Tensor] = None,
-            token_type_ids: Optional[torch.Tensor] = None,
-            position_ids: Optional[torch.Tensor] = None,
-            head_mask: Optional[torch.Tensor] = None,
-            inputs_embeds: Optional[torch.Tensor] = None,
-            labels: Optional[torch.Tensor] = None,
-            output_attentions: Optional[bool] = None,
-            output_hidden_states: Optional[bool] = None,
-            return_dict: Optional[bool] = None,
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
     ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
 
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
 
         AQUASwindowsvectors = []
         AQUASnumberwindows = 0
-        for item in input_ids: 
+        for item in input_ids:
             if len(item) > 512:
                 windows = sliding_window(item)
                 for window in windows:
-                    #finetune_windw = fine_tune_BERT(window)
-                    #vector.append(finetune_windw)
+                    # finetune_windw = fine_tune_BERT(window)
+                    # vector.append(finetune_windw)
 
                     # replaced "self" by "window"
                     outputs = window.Sbert(
@@ -114,18 +135,17 @@ class AQUASSlidingBERT(BertForSequenceClassification):
                         output_hidden_states=output_hidden_states,
                         return_dict=return_dict,
                     )
-                # get the vector
+                    # get the vector
                     pooled_output = outputs[1]
 
                     AQUASwindowsvectors.append(pooled_output)
                     AQUASnumberwindows += 1
-                #sum and mean
-                #AQUASpooled_output = [sum(i) for i in AQUASwindowsvectors] / AQUASnumberwindows
+                # sum and mean
+                # AQUASpooled_output = [sum(i) for i in AQUASwindowsvectors] / AQUASnumberwindows
                 AQUASpooled_output = torch.stack(AQUASwindowsvectors, dim=0).mean(dim=0)
 
-
             else:
-                #replaced "self" by "item"
+                # replaced "self" by "item"
                 outputs = item.AQUASbert(
                     input_ids,
                     attention_mask=attention_mask,
@@ -139,7 +159,6 @@ class AQUASSlidingBERT(BertForSequenceClassification):
                 )
                 AQUASpooled_output = outputs[1]
 
-
         pooled_output = self.dropout(AQUASpooled_output)
         logits = self.classifier(pooled_output)
 
@@ -148,7 +167,9 @@ class AQUASSlidingBERT(BertForSequenceClassification):
             if self.config.problem_type is None:
                 if self.num_labels == 1:
                     self.config.problem_type = "regression"
-                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                elif self.num_labels > 1 and (
+                    labels.dtype == torch.long or labels.dtype == torch.int
+                ):
                     self.config.problem_type = "single_label_classification"
                 else:
                     self.config.problem_type = "multi_label_classification"
@@ -175,16 +196,16 @@ class AQUASSlidingBERT(BertForSequenceClassification):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
-        print('sliding window: check!')
-
+        print("sliding window: check!")
 
 
 def train_epoch(model, optimizer, train_inputs, train_labels, train_masks):
-    #optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08, clipnorm=1.0)
+    # optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08, clipnorm=1.0)
 
     # bisher: batch size 1, mehr spaeter
-    train_loader = torch.data.DataLoader(zip(train_inputs, train_labels, train_masks), batch_size=1, shuffle=True)
-
+    train_loader = torch.data.DataLoader(
+        zip(train_inputs, train_labels, train_masks), batch_size=1, shuffle=True
+    )
 
     # training for one epoch
     for batch in train_loader:
@@ -192,50 +213,100 @@ def train_epoch(model, optimizer, train_inputs, train_labels, train_masks):
 
         # batch auseinanerfriemeln
         batch_inputs, batch_labels, batch_masks = batch
-        output = model(input_id=batch_inputs, labels=batch_labels, attention_mask=batch_masks)
+        output = model(
+            input_id=batch_inputs, labels=batch_labels, attention_mask=batch_masks
+        )
 
         # output: SequenceClassifierOutput
-        loss = output['loss'] # oder output.loss
+        loss = output["loss"]  # oder output.loss
 
         loss.backward()
         optimizer.step()
 
-
-    #model.compile(optimizer=optimizer, loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
-    print('model trained')
+    # model.compile(optimizer=optimizer, loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+    print("model trained")
     return model
 
 
 def evaluate_model(model, val_inputs, val_masks, val_labels):
     # Evaluate  model
-    results = model.evaluate([val_inputs, val_masks], val_labels, batch_size=8)
+    val_loader = torch.data.DataLoader(
+        zip(val_inputs, val_masks),
+        batch_size=1,
+        shuffle=False,  # Never change to True, else all will break
+    )
+
+    predictions = []
+    with torch.no_grad():
+        model.eval()
+        for batch_input, batch_mask in val_loader:
+            outputs = model(input_id=batch_input, attention_mask=val_mask)
+            logits = outputs[1]
+            predicted_class = torch.argmax(logits, dim=1)
+
+            predictions.append(predicted_class)
+
+    predictions = torch.stack(predictions)
+    # Sliding
     output = model.predict([val_inputs, val_masks])
+
     predicted_labels = output.logits.argmax(axis=1)
-    f1 = f1_score(val_labels.argmax(axis=1), predicted_labels, average='weighted')
-    print('Validation Loss: {:.4f} Accuracy: {:.4f} F1-score: {:.4f}'.format(*results, f1))
+
+    accuracy = (predictions == val_labels).mean()
+
+    f1 = f1_score(val_labels, predictions, average="weighted")
+
+    return accuracy, f1
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_file_csv')
+    parser.add_argument("input_file_csv")
     args = parser.parse_args()
+
+    learning_rate = 5e-5
+    epochs = 10
+
+    run = wandb.init(
+        # Set the project where this run will be logged
+        project="AQUAS",
+        # Track hyperparameters and run metadata
+        config={
+            "learning_rate": learning_rate,
+            "epochs": epochs,
+        },
+    )
 
     texts, labels = load_dataset(args.input_file_csv)
     tokens = tokenize(texts)
     labels_conv = convert_labels(labels)
     split_ratio = calc_split_ratio(labels_conv)
-    train_inputs, val_inputs, train_masks, val_masks, train_labels, val_labels= split_train_val_data(tokens, split_ratio, labels_conv)
-
+    (
+        train_inputs,
+        val_inputs,
+        train_masks,
+        val_masks,
+        train_labels,
+        val_labels,
+    ) = split_train_val_data(tokens, split_ratio, labels_conv)
 
     # OUR AQUASBert INIT
-    model = AQUASSlidingBERT.from_pretrained('bert-base-uncased', num_labels=3)  # BioBERT statt bert-base-uncased
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+    model = AQUASSlidingBERT.from_pretrained(
+        BERT_MODEL_IDENTIFIER, num_labels=3
+    )  # BioBERT statt bert-base-uncased
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    wandb.watch(model)
 
     # each loop is one epoch
-    for epoch in range(10):
+    for epoch in range(epochs):
         train_epoch(model, optimizer, train_inputs, train_labels, train_masks)
+        acc, f1 = evaluate_model(model, val_inputs, val_masks, val_labels)
 
-    evaluate_model(model, val_inputs, val_masks, val_labels)
+        wandb.log({"accuracy": acc, "f1": f1})
+
+        print(f"[{epoch+1}] Accuracy: {acc:.4f} F1-score: {f1:.4f}")
 
 
-main()
+if __name__ == "__main__":
+    main()
